faf6eaad3a0504eb0b6c70514ae4200b26866591
lauCodx <lauphix1@gmail.com>
2024-01-27 16:23:39 +0100

    addded the median method

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Task-4--Descriptive-statistics-code
+
+- The measures of central tendency
+  1. Mean
+  2. Median
+  3. mode
--- Stats.js
+++ Stats.js
@@ -4,12 +4,16 @@ class centralTendency {
     }
 
     mean(){
-        const sumValues = this.x.reduce((arr, val) =>(arr + val), 0 )
+        const sumValues = this.x.reduce((arr, val) =>(arr + val), 0 ) // Adds all the values in the array using the reduce() function
         return sumValues/this.x.length;
     }
 
+    // Median is all about finding the middle number.
     median (){
-        sortedData = sort(this.x);
+        const sortedData = this.x.sort() // arranges the numbers in accending order using the sort() function.
+        const getMiddleNumber = Math.floor(this.x.length / 2);
+
+        return sortedData.length % 2=== 1 ? sortedData[getMiddleNumber] : (sortedData[getMiddleNumber -1] + sortedData[getMiddleNumber]) / 2;
         
     }
 }
@@ -24,6 +28,7 @@ class centralTendency {
 
 
 // MY CONSOLES
-const stats1 = new centralTendency([1,2,3,4,5,6,7,8,9,10]);
+const stats1 = new centralTendency([1,2,3,4,5,6,7,8,9]);
 
 console.log(stats1.mean());
+console.log(stats1.median());
